d090c13397c4c29f3a28d72f1b16abc4a90013fe
Ariel Rodriguez <arielsvn@gmail.com>
2017-04-17 11:02:24 -0400

    ...

--- hello/predictions.py
+++ hello/predictions.py
@@ -1,15 +1,12 @@
 
 
-import random
-
-
 # Function used to retrieve a list of business given some filters
 # City : name of the city
 # categories: array with the types of business eg: ['restaurants', 'bowling', ...]
 # Returns: an array with the ids of the businesses
 def get_users(city, categories):
     ids = ['KpkOkG6RIf4Ra25Lhhxf1A', 'bQ7fQq1otn9hKX-gXRsrgA', 'r1NUhdNmL6yU9Bn-Yx6FTw', 'aW3ix1KNZAvoM8q-WghA3Q', 'YOo-Cip8HqvKp_p9nEGphw']
-    return random.sample(ids, len(ids))
+    return ids
 
 
 
